5af202c9a6cd65d9f7f6d681623fb8e3c569a1cd
rohit-2002 <rohitbaba198@gmail.com>
2025-08-13 16:00:45 +0530

    added the production code in server.js file

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import employeeRoutes from "./src/routes/employees.js";
 import leaveRoutes from "./src/routes/leaves.js";
 import errorHandler from "./src/middlewares/errorHandler.js";
 
-dotenv.config();
-const app = express();
+if (process.env.NODE_ENV !== "production") {
+  import("dotenv").then((dotenv) => dotenv.config());
+}
 
+const app = express();
 app.use(bodyParser.json());
+
 app.use("/api/employees", employeeRoutes);
 app.use("/api/leaves", leaveRoutes);
 app.use(errorHandler);
@@ -17,7 +19,7 @@ app.use(errorHandler);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error(err));
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
